c262f02ee7eeededa794c5df08c47f396e144e83
JerecTharen <kf7ags@gmail.com>
2020-01-01 17:49:41 -0500

    initial dependencies and area structure

---
+++ OccupationCyberBounty/Areas/WoWBounties/WoWBountiesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OccupationCyberBounty.Areas.WoWBounties
+{
+    public class WoWBountiesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WoWBounties";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WoWBounties_default",
+                "WoWBounties/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
